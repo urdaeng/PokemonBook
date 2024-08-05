3ba5033623a8d7b651faacdd4721db60dbbe944c
urdaeng <gni711@naver.com>
2024-08-05 14:44:20 +0900

    PokemonDetail 구조체 생성

--- Model/PokemonDetail.swift
+++ Model/PokemonDetail.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PokemonDetailResponse: Codable {
+    
+    let results: [PokemonDetail]
+    // results는 안에 [PokemonDetail]이라는 타입이 들어오도록.
+}
+
+// 구조체 생성
+struct PokemonDetail: Codable {
+    
+    // 원하는 데이터 가져오기
+    let id : Int?               // 아이디
+    let types: String?          // 타입
+    let height: Double?         // 키
+    let weight: Double?         // 몸무게
+    
+}
