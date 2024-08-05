67d5bd6e19a03507c6ef34e2b353a97c4d1d3084
urdaeng <gni711@naver.com>
2024-08-05 14:33:42 +0900

    Pokemon 구조체 생성

---
+++ Model/Pokemon.swift
@@ -0,0 +1,23 @@
+//
+//  Pokemon.swift
+//  PokemonBook
+//
+//  Created by 강유정 on 8/5/24.
+//
+
+import Foundation
+
+struct PokemonResponse: Codable {
+    
+    let results: [Pokemon]
+    // results는 안에 [Pokemon]이라는 타입이 들어오도록.
+}
+
+// 구조체 생성
+struct Pokemon: Codable {
+    
+    // 원하는 데이터 가져오기
+    let name: String?          // 포켓몬 이름
+    let url: String?           // 포켓몬 상세 정보를 얻기 위한 URL
+    
+}
---
+++ Model/PokemonDetail.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDetail.swift
+//  PokemonBook
+//
+//  Created by 강유정 on 8/5/24.
+//
+
+import Foundation
